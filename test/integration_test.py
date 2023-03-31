0c8d3403c9c95c096f9d6b143c79c062b671373c
opas95 <etolorcallister@gmail.com>
2023-03-31 05:10:14 +0300

    new changes for test

--- test/integration_test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def test_integration():
-    url = 'http://localhost:5000/'
-    response = requests.get(url)
-    assert response.status_code == 200
-    assert 'Hello, World!' in response.text
-
---
+++ test/test_integration.py
@@ -0,0 +1,9 @@
+from my_app import app
+
+def test_homepage():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'I am almost a Devops Engineer!' in response.data
+
+
